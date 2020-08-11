740ab16ade41973e7ef2df489e68932a8d17a260
Nick La Rosa <nick@nicklarosa.net>
2020-08-11 21:44:21 +1000

    re-arranged setup file without bugs

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 curl -sS https://wordpress.org/latest.zip > file.zip && \
 unzip file.zip                                       && \
 rm file.zip
+mkdir themes && \
+mkdir plugins && \
+mkdir docker-compose && \
+mkdir docker-compose/mysql && \
 cp -r ./wordpress/wp-content/themes/* ./themes/ && \
 cp -r ./wordpress/wp-content/plugins/* ./plugins/ && \
 cp ./wordpress/wp-config-sample.php ./wordpress/wp-config.php && \
-mkdir themes && \
-mkdir plugins && \
 sed -i 's/password_here/wordpress_password/g' ./wordpress/wp-config.php && \
 sed -i 's/database_name_here/wordpress_db/g' ./wordpress/wp-config.php && \
 sed -i 's/username_here/wordpress_user/g' ./wordpress/wp-config.php
